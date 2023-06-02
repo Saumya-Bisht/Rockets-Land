d984f1db089f8f950e2bdeb089f9ceb734149658
Saumya-Bisht <saumyabisht05@gmail.com>
2023-06-02 18:06:48 +0530

    updated

--- src/Components/Filtering.jsx
+++ src/Components/Filtering.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 function Filtering({onH,onE,onL}) {
   return (
-    <div className='filter-wrapper'>
+    <div>
+      <label style={{fontFamily:"Dancing Script",textAlign:"center",color:"white",margin:"20px",padding:"10px",fontSize:"larger"}}>Select a height to see the rockets of the exact selected height-</label>
         <select name='height' className='filter' onChange={onH}>
             <option value="0">---Select Height---</option>
             <option value="100"> Greater than 100 Feet</option>
